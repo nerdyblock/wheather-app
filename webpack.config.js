82adf57b6efb4d0ac52fb3232be7977a758f32a9
Akash Pal <creativeakashpal@gmail.com>
2023-01-25 18:58:20 +0530

    added dotenv-webpack package

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
@@ -47,5 +48,6 @@ module.exports = {
       filename: "index.html",
       template: path.resolve(__dirname, "src/temp.html"),
     }),
+    new Dotenv(),
   ],
 };
