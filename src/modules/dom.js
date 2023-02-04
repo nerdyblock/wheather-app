a4c2e4c41b75250103c8d277d3367f0b897703de
Akash Pal <creativeakashpal@gmail.com>
2023-02-04 22:38:37 +0530

    added daily weather display

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,4 +1,23 @@
-export default function uiShowCurrentTemperature(value) {
+import { getDailyWeather } from "./api";
+
+export default function uiShowCurrentTemperature(currentTemp) {
   const currentTempDisplay = document.getElementById("current-temp");
-  currentTempDisplay.textContent = value;
+  const humidityDisplay = document.querySelector(".humidity");
+  const windSpeedDisplay = document.querySelector(".wind");
+  windSpeedDisplay.textContent = currentTemp.wind.speed;
+  humidityDisplay.textContent = currentTemp.main.humidity;
+  currentTempDisplay.textContent = currentTemp.main.temp;
+}
+
+export async function uiDailyWeather() {
+  const dailyWeather = await getDailyWeather();
+  dailyWeather.forEach((item, index) => {
+    const day = document.querySelector(`.day${index + 1}`);
+    const maxTemp = document.querySelector(`.max-temp${index + 1}`);
+    const minTemp = document.querySelector(`.min-temp${index + 1}`);
+
+    day.textContent = item.date;
+    maxTemp.textContent = item.maxTemp;
+    minTemp.textContent = item.minTemp;
+  });
 }
