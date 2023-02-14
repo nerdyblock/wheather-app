e929f0c26a7646ac605b7f98e8b42a8703f31a06
Akash Pal <creativeakashpal@gmail.com>
2023-02-14 22:16:18 +0530

    added search city

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import randomFunction from "./modules/api";
-import uiShowCurrentTemperature from "./modules/dom";
+import { getDailyWeather, setLatLongUrl } from "./modules/api";
+import uiShowCurrentTemperature, { uiDailyWeather } from "./modules/dom";
+import "./styles/style.css";
 
-async function displayCurrentTemp() {
-  const currentTemp = await randomFunction();
-  uiShowCurrentTemperature(currentTemp.main.temp);
-}
+const searchCity = document.getElementById("search");
+document.querySelector("body").style.visibility = "hidden";
 
-displayCurrentTemp();
-// Get current date
-const date = new Date();
+searchCity.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    setLatLongUrl(e.target.value);
+    e.target.value = "";
+    uiShowCurrentTemperature();
+    uiDailyWeather();
+    getDailyWeather();
+  }
+});
 
-// Add five days to current date
-// date.setDate(date.getDate() + 5);
+document.getElementById("search-button").addEventListener("click", () => {
+  setLatLongUrl(searchCity.value);
+  searchCity.value = "";
+  uiShowCurrentTemperature();
+  uiDailyWeather();
+  getDailyWeather();
+});
 
-// get day in form of number 0-6 sun-sat
-console.log(date.getDay());
-// get current date and day in Mon Jan 23 2023 18:22:25 GMT+0530 (India Standard Time) form
-console.log(date);
+window.addEventListener("DOMContentLoaded", () => {
+  uiShowCurrentTemperature();
+  uiDailyWeather();
+  getDailyWeather();
+});
